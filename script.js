5d037c1f8a02cb55f6c6cf0148dafe08806fbb76
Jam_Al <jambiebs@gmail.com>
2023-01-20 12:39:31 +0530

    final3

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-requestFullScreen();
-let disp = document.querySelector(".display");
-
-console.dir(disp);
+const result = document.getElementById("result");
+resultg.addEventListener('keypress', function(e){
+    if(e.key === 'Enter'){
+        e.preventDefault();
+        result.value = eval(result.value);
+    } 
+//      Uncaught TypeError: Cannot read properties of undefined (reading 'value')
+//     at HTMLInputElement.<anonymous> (script.js:6:36)
+// (anonymous) @ script.js:6
+});
