7bc8b95bea1baa55921559a7933dd3259b30ef8f
Jam_Al <jambiebs@gmail.com>
2023-01-20 12:51:27 +0530

    ec

--- script.js
+++ script.js
@@ -3,8 +3,5 @@ result.addEventListener('keypress', function(e){
     if(e.key === 'Enter'){
         e.preventDefault();
         result.value = eval(result.value);
-    } 
-//      Uncaught TypeError: Cannot read properties of undefined (reading 'value')
-//     at HTMLInputElement.<anonymous> (script.js:6:36)
-// (anonymous) @ script.js:6
+    }
 });
